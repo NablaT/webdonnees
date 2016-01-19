e3b1706cb38d2dbcb6c0498a03cbc916ac522cb2
NablaT <remi.pourtier@laposte.net>
2016-01-19 15:59:13 +0100

    Adding method loadDoc

--- sparqlrequest.js
+++ sparqlrequest.js
@@ -1,16 +1,52 @@
-sparqlRequest("fr.dbpedia.org",
+var resp=sparqlRequest("fr.dbpedia.org",
+    "SELECT DISTINCT ?value ?year WHERE { <http://fr.dbpedia.org/resource/Aerosmith> dbpedia-owl:genre ?value . <http://fr.dbpedia.org/resource/Aerosmith> dbpedia-owl:activeYearsStartYear ?year}")
+
+loadDoc("fr.dbpedia.org",
     "SELECT DISTINCT ?value ?year WHERE { <http://fr.dbpedia.org/resource/Aerosmith> dbpedia-owl:genre ?value . <http://fr.dbpedia.org/resource/Aerosmith> dbpedia-owl:activeYearsStartYear ?year}")
 
 function sparqlRequest(siteDomain, query) {
     var url = "http://" + siteDomain + "/sparql?query=";
-    url += encodeURIComponent(query);
+    url += encodeURIComponent(query) + "&format=text";
     console.log("url: ", url);
-    $.ajax({
-        dataType: 'html',
+
+    var outputs;
+    $.get(url, function (data) {
+        console.log(data);
+    })
+
+    var resp=$.ajax({
+        dataType: 'text',
         url: url,
+        type: 'GET',
+
+
         success: function (data) {
-            alert('success: ' + data.results.bindings.length + ' results');
-            console.log(data);
+            console.log("Data: ",data);
+            return data;
         }
     });
+
+
+}
+
+function getList(){
+    alert(document.getElementById("demo").getAttributeNode());
+    document.getElementById("demo").innerHTML;
+}
+
+
+function loadDoc(siteDomain, query) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xhttp.responseText;
+            //var file=jQuery.parseJSON(xhttp.responseText);
+
+        }
+    };
+    var url = "http://" + siteDomain + "/sparql?query=";
+    url += encodeURIComponent(query) + "&format=text";
+    xhttp.open("GET", url, true);
+    xhttp.getAllResponseHeaders();
+    xhttp.send();
 }
