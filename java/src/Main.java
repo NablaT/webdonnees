ceebf5a42ff749d15f08437abce55ebe76fd97c1
NablaT <remi.pourtier@laposte.net>
2016-02-09 14:03:51 +0100

    Cleaning workspace

--- java/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by jonathan on 26/01/16.
- */
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, world !");
-    }
-}
--- web/app/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'npm/angular';
-import TestController from './test.controller';
-import kohakuHighchartsDirective from './highcharts.directive';
-import HighchartsService from './highcharts.service';
-
-angular.module('kohaku', [])
-  .controller('TestController', TestController)
-  .directive('kohakuHighcharts', kohakuHighchartsDirective)
-  .factory('HighchartsService', HighchartsService);
